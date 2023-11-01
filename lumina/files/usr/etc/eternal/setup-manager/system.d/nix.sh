85599938570bf64c598f2842fafced9337ddbb82
Robert Sturla <robertsturla@outlook.com>
2023-11-01 16:55:15 +0000

    Add correct file permissions to devbox bash eval scripts

--- lumina/files/usr/etc/eternal/setup-manager/system.d/nix.sh
+++ lumina/files/usr/etc/eternal/setup-manager/system.d/nix.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
-echo 'eval "$(devbox global shellenv --init-hook)"' >> /etc/bashrc.d/devbox.sh
+
+if [ ! -f /etc/bashrc.d/devbox.sh ]; then
+  echo 'eval "$(devbox global shellenv --init-hook)"' >> /etc/bashrc.d/devbox.sh
+  chmod +x /etc/bashrc.d/devbox.sh
+fi
