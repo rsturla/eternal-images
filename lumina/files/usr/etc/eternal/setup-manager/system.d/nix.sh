c76ec1632ea36c0654d9791d34191b93528f0a4e
Robert Sturla <robertsturla@outlook.com>
2023-11-01 16:52:00 +0000

    Add devbox to shell after installing Nix

--- lumina/files/usr/etc/eternal/setup-manager/system.d/nix.sh
+++ lumina/files/usr/etc/eternal/setup-manager/system.d/nix.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
+echo 'eval "$(devbox global shellenv --init-hook)"' >> /etc/bashrc.d/devbox.sh
