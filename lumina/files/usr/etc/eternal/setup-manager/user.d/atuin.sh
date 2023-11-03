bde1f8adbeedff7f933d036667974a53a5a6900b
Robert Sturla <robertsturla@outlook.com>
2023-11-03 18:04:55 +0000

    Remove comment on atuin auto script

--- lumina/files/usr/etc/eternal/setup-manager/user.d/atuin.sh
+++ lumina/files/usr/etc/eternal/setup-manager/user.d/atuin.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-# Copy /etc/eternal/config/atuin.toml to $HOME/.config/atuin/config.toml if it doesn't exist
 if [ ! -f $HOME/.config/atuin/config.toml ]; then
   echo "Copying /etc/eternal/config/atuin.toml to $HOME/.config/atuin/config.toml as it doesn't exist"
   mkdir -p $HOME/.config/atuin
