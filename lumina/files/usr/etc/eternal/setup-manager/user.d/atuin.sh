060e1093375e988018d98032bb2c843eb03ba845
Robert Sturla <robertsturla@outlook.com>
2023-11-03 18:31:28 +0000

    feature(lumina): install atuin shell (#62)

---
+++ lumina/files/usr/etc/eternal/setup-manager/user.d/atuin.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+if [ ! -f $HOME/.config/atuin/config.toml ]; then
+  echo "Copying /etc/eternal/config/atuin.toml to $HOME/.config/atuin/config.toml as it doesn't exist"
+  mkdir -p $HOME/.config/atuin
+  cp /etc/eternal/config/atuin.toml $HOME/.config/atuin/config.toml
+  exit 0
+fi
+
+echo "Skipping copying /etc/eternal/config/atuin.toml to $HOME/.config/atuin/config.toml as it already exists"
