ec09d72e86e2d9c814e054633c7ac260b55ed8eb
Robert Sturla <robertsturla@outlook.com>
2023-11-24 14:56:36 +0000

    feature(lumina): add nix binaries to sudoers (#72)

--- lumina/files/usr/etc/eternal/setup-manager/system.d/nix.sh
+++ lumina/files/usr/etc/eternal/setup-manager/system.d/nix.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
+
+tee /etc/sudoers.d/nix-sudo-env <<EOF
+    Defaults  secure_path = /nix/var/nix/profiles/default/bin:/nix/var/nix/profiles/default/sbin:$(printenv PATH)
+EOF
