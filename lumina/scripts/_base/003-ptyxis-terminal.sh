a7599605df6f10b607ede1d28cda163a702c565a
Robert Sturla <robertsturla@outlook.com>
2024-04-09 14:23:13 +0100

    Enable Fedora 40 builds

--- lumina/scripts/_base/003-ptyxis-terminal.sh
+++ lumina/scripts/_base/003-ptyxis-terminal.sh
@@ -2,15 +2,42 @@
 
 set -euox pipefail
 
+FEDORA_VERSION=""
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --version)
+      FEDORA_VERSION="$2"
+      shift 2
+      ;;
+    *)
+      echo "Unknown argument: $1"
+      exit 1
+      ;;
+  esac
+done
+
+if [[ -z "$FEDORA_VERSION" ]]; then
+  echo "--version flag is required"
+  exit 1
+fi
+
 wget https://copr.fedorainfracloud.org/coprs/ublue-os/staging/repo/fedora-$(rpm -E %fedora)/ublue-os-staging-fedora-$(rpm -E %fedora).repo -O /etc/yum.repos.d/_copr_ublue-os_staging.repo
 
 rpm-ostree override replace \
   --experimental \
   --from repo=copr:copr.fedorainfracloud.org:ublue-os:staging \
-    gtk4 \
     vte291 \
-    vte-profile \
-    libadwaita
+    vte-profile
+
+# Fedora 40 already has the required versions of gtk4 and libadwaita
+if [[ "$FEDORA_VERSION" == "39" ]]; then
+  rpm-ostree override replace \
+    --experimental \
+    --from repo=copr:copr.fedorainfracloud.org:ublue-os:staging \
+      gtk4 \
+      libadwaita
+fi
 
 rpm-ostree install ptyxis
 
