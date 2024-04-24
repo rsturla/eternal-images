eedad167d650dd216c590fa19f5a4b08bc6147da
Robert Sturla <robertsturla@outlook.com>
2024-04-24 23:12:45 +0100

    Revert "feature: install OpenVPN3 devsnapshots (#186)"
    This reverts commit b5efb6b45fd67f6474802c6e448552bf7a6fc030.

--- lumina/scripts/_base/010-install-openvpn3.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euox pipefail
-
-wget https://copr.fedorainfracloud.org/coprs/dsommers/openvpn3-devsnapshots/repo/fedora-$(rpm -E %fedora)/dsommers-openvpn3-devsnapshots-fedora-$(rpm -E %fedora).repo -O /etc/yum.repos.d/_copr_dsommers-devsnapshots.repo
-
-rpm-ostree install openvpn3
-
-rm -rf /etc/yum.repos.d/_copr_dsommers-devsnapshots.repo
