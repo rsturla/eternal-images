795c4ff26ee54c0d978a87aaabc550e2143e2516
Robert Sturla <robertsturla@outlook.com>
2024-05-01 17:27:56 +0100

    Add cockpit Quadlet and packages

---
+++ lumina/scripts/_base/010-setup-cockpit.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euox pipefail
+
+rpm-ostree install \
+  cockpit-machines \
+  cockpit-networkmanager \
+  cockpit-ostree \
+  cockpit-pcp \
+  cockpit-podman \
+  cockpit-selinux \
+  cockpit-storaged \
+  cockpit-system
