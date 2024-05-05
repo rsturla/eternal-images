8d01c1575c2a39e9203025b9d4ad01f7807159a7
Robert Sturla <robertsturla@outlook.com>
2024-05-05 18:19:55 +0100

    chore: delete gsconnect (#207)
    Delete gsconnect

--- lumina/scripts/_silverblue/001-gnome-shell.sh
+++ lumina/scripts/_silverblue/001-gnome-shell.sh
@@ -8,8 +8,6 @@ rpm-ostree install \
   gnome-shell-extension-appindicator \
   gnome-shell-extension-dash-to-dock \
   gnome-shell-extension-blur-my-shell \
-  gnome-shell-extension-gsconnect \
-  nautilus-gsconnect \
   yaru-theme \
   gnome-tweaks \
   libcanberra-gtk3 \
