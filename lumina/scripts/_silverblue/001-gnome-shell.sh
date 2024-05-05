af80014e4b21c87686d4dc4b0cf02aee8af0a175
Robert Sturla <robertsturla@outlook.com>
2024-05-05 18:35:50 +0100

    Add searchlight

--- lumina/scripts/_silverblue/001-gnome-shell.sh
+++ lumina/scripts/_silverblue/001-gnome-shell.sh
@@ -8,6 +8,7 @@ rpm-ostree install \
   gnome-shell-extension-appindicator \
   gnome-shell-extension-dash-to-dock \
   gnome-shell-extension-blur-my-shell \
+  gnome-shell-extension-search-light \
   yaru-theme \
   gnome-tweaks \
   libcanberra-gtk3 \
