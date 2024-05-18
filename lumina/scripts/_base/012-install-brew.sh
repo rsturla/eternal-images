de3ac3b3769bd5c065b0ed66a2e2e7d4f6af0813
Robert Sturla <robertsturla@outlook.com>
2024-05-19 00:24:13 +0100

    Simple install

---
+++ lumina/scripts/_base/012-install-brew.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+set -xeou pipefail
+
+# Convince the installer we are in CI
+if [[ ! -f /.dockerenv ]]; then
+    touch /.dockerenv
+fi
+
+# Make these so script will work
+mkdir -p /var/home
+mkdir -p /var/roothome
+
+# Install brew
+/usr/libexec/brew-install
+
+# Copy to image and own by UID 1000
+cp -R /home/linuxbrew /usr/share/homebrew
+
+# Enable Systemd services
+systemctl enable brew-setup.service
+systemctl enable brew-upgrade.timer
+systemctl enable brew-update.timer
