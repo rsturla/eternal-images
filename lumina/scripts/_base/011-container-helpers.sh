dcd91625ff99c335646e04f6e6f7c534760590c0
Robert Sturla <robertsturla@outlook.com>
2024-05-21 23:56:38 +0100

    Download and install manually

--- lumina/scripts/_base/011-container-helpers.sh
+++ lumina/scripts/_base/011-container-helpers.sh
@@ -3,5 +3,13 @@
 set -euox pipefail
 
 rpm-ostree install \
-    crun-vm \
-    amazon-ecr-credential-helper
+    crun-vm 
+
+# Download ECR Credential Helper
+ARCH=$(uname -m)
+[ "$ARCH" = "arm" ] && ARCH="arm64"
+
+TAG_NAME=$(curl -s https://api.github.com/repos/awslabs/amazon-ecr-credential-helper/releases/latest | jq -r ".tag_name" | cut -c 2-)
+
+curl -Lo /usr/bin/docker-credential-ecr-login https://amazon-ecr-credential-helper-releases.s3.us-east-2.amazonaws.com/${TAG_NAME}/linux-${ARCH}/docker-credential-ecr-login
+chmod +x /usr/bin/docker-credential-ecr-login
