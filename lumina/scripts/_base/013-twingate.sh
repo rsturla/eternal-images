302b1eeadc2fa99cda3ac5666e18208bd770b0dc
Robert Sturla <robertsturla@outlook.com>
2024-05-29 23:42:38 +0100

    Install Twingate client

---
+++ lumina/scripts/_base/013-twingate.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+set -euox pipefail
+
+# Setup repo
+cat << EOF > /etc/yum.repos.d/twingate.repo
+[twingate]
+name=Twingate
+baseurl=https://packages.twingate.com/rpm/
+enabled=1
+gpgcheck=0
+EOF
+
+rpm-ostree install twingate
