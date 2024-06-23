f2e464b5dbf295166ee09225d224b224e4288014
Robert Sturla <robertsturla@outlook.com>
2024-06-23 18:26:48 +0100

    Remove explicit vte deps

--- lumina/scripts/_base/003-ptyxis-terminal.sh
+++ lumina/scripts/_base/003-ptyxis-terminal.sh
@@ -7,12 +7,6 @@ FEDORA_VERSION=$VERSION_ID
 
 wget https://copr.fedorainfracloud.org/coprs/ublue-os/staging/repo/fedora-$(rpm -E %fedora)/ublue-os-staging-fedora-$(rpm -E %fedora).repo -O /etc/yum.repos.d/_copr_ublue-os_staging.repo
 
-rpm-ostree override replace \
-  --experimental \
-  --from repo=copr:copr.fedorainfracloud.org:ublue-os:staging \
-    vte291 \
-    vte-profile
-
 # Fedora 40 already has the required versions of gtk4 and libadwaita
 if [[ "$FEDORA_VERSION" == "39" ]]; then
   rpm-ostree override replace \
