b64497975571142a047b956092492e868027e842
Robert Sturla <robertsturla@outlook.com>
2024-06-29 13:38:10 +0100

    feature: install additional VSCode extensions (#252)
    * Install additional VSCode extensions
    
    * Add vscode-icons
    
    * Add default vscode settings

--- lumina/files/_base/usr/etc/eternal/setup-manager/user.d/vscode.sh
+++ lumina/files/_base/usr/etc/eternal/setup-manager/user.d/vscode.sh
@@ -1,4 +1,32 @@
 #!/usr/bin/env bash
 
-code --install-extension ms-vscode-remote.vscode-remote-extensionpack
-code --install-extension jetpack-io.devbox
+# Define an array of VS Code extensions to be installed
+extensions=(
+  ms-vscode-remote.vscode-remote-extensionpack
+  jetpack-io.devbox
+  usernamehw.errorlens
+  editorconfig.editorconfig
+  github.copilot
+  hashicorp.hcl
+  hashicorp.terraform
+  vscode-icons-team.vscode-icons
+)
+
+# Function to install a single extension
+install_extension() {
+  local extension=$1
+  echo "Installing $extension..."
+  code --install-extension "$extension"
+  if [[ $? -ne 0 ]]; then
+    echo "Error installing $extension" >&2
+  else
+    echo "$extension installed successfully."
+  fi
+}
+
+# Iterate over the extensions array and install each one
+for extension in "${extensions[@]}"; do
+  install_extension "$extension"
+done
+
+echo "All extensions installation attempted."
