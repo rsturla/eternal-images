8ed22ab6af5be8194d07e98e64ff39bc1507497d
Robert Sturla <robertsturla@outlook.com>
2024-07-01 10:38:07 +0100

    feature: download monaspace fonts (#255)
    * Download monaspace fonts
    
    * Build font information
    
    * Copy fonts to subdirectory
    
    * Quiet unzip
    
    * Use monaspace font in VSCode
    
    * Only display errors in quadlets

--- lumina/scripts/_base/999-dryrun-gen-quadlets.sh
+++ lumina/scripts/_base/999-dryrun-gen-quadlets.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-set -euox pipefail
+set -euo pipefail
 
-# Test Quadlets
-QUADLET_UNIT_DIRS=/usr/etc/containers/systemd /usr/lib/systemd/system-generators/podman-system-generator --dryrun
-QUADLET_UNIT_DIRS=/usr/etc/containers/systemd/users /usr/lib/systemd/system-generators/podman-system-generator --user --dryrun
+# Test Quadlets
+# To avoid incredibly long outputs, we redirect the output to /dev/null and log only errors
+QUADLET_UNIT_DIRS=/usr/etc/containers/systemd /usr/lib/systemd/system-generators/podman-system-generator --dryrun > /dev/null 2>>error.log
+QUADLET_UNIT_DIRS=/usr/etc/containers/systemd/users /usr/lib/systemd/system-generators/podman-system-generator --user --dryrun > /dev/null 2>>error.log
