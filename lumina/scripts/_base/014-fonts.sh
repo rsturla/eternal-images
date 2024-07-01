329a855740c8b3475dec0fbb31464309e593920b
Robert Sturla <robertsturla@outlook.com>
2024-07-01 10:09:26 +0100

    Copy fonts to subdirectory

--- lumina/scripts/_base/014-fonts.sh
+++ lumina/scripts/_base/014-fonts.sh
@@ -4,10 +4,11 @@ set -euox pipefail
 
 # GitHub Monaspace
 DOWNLOAD_URL=$(curl https://api.github.com/repos/githubnext/monaspace/releases/latest | jq -r '.assets[] | select(.name| test(".*.zip$")).browser_download_url')
-curl -Lo /tmp/monospace-font.zip $DOWNLOAD_URL
+curl -Lo /tmp/monaspace-font.zip $DOWNLOAD_URL
 
-unzip -o /tmp/monospace-font.zip -d /tmp/monospace-font
-mv /tmp/monospace-font/monaspace-v*/fonts/otf/* /usr/share/fonts/
-mv /tmp/monospace-font/monaspace-v*/fonts/variable/* /usr/share/fonts/
+unzip -o /tmp/monaspace-font.zip -d /tmp/monaspace-font
+mkdir -p /usr/share/fonts/monaspace
+mv /tmp/monaspace-font/monaspace-v*/fonts/otf/* /usr/share/fonts/monaspace/
+mv /tmp/monaspace-font/monaspace-v*/fonts/variable/* /usr/share/fonts/monaspace/
 
-fc-cache -f /usr/share/fonts/
+fc-cache -f /usr/share/fonts/monaspace
