a45ecd95a40cf50f829a6fefcdd0546ba58b981e
Robert Sturla <robertsturla@outlook.com>
2024-07-01 09:58:07 +0100

    Build font information

--- lumina/scripts/_base/014-fonts.sh
+++ lumina/scripts/_base/014-fonts.sh
@@ -2,14 +2,12 @@
 
 set -euox pipefail
 
-rpm-ostree install \
-  $(curl https://api.github.com/repos/charmbracelet/mods/releases/latest | jq -r '.assets[] | select(.name| test(".*.x86_64.rpm$")).browser_download_url')
-
-
-# TODO: Dynamically fetch the latest release
+# GitHub Monaspace
 DOWNLOAD_URL=$(curl https://api.github.com/repos/githubnext/monaspace/releases/latest | jq -r '.assets[] | select(.name| test(".*.zip$")).browser_download_url')
 curl -Lo /tmp/monospace-font.zip $DOWNLOAD_URL
 
 unzip -o /tmp/monospace-font.zip -d /tmp/monospace-font
 mv /tmp/monospace-font/monaspace-v*/fonts/otf/* /usr/share/fonts/
 mv /tmp/monospace-font/monaspace-v*/fonts/variable/* /usr/share/fonts/
+
+fc-cache -f /usr/share/fonts/
