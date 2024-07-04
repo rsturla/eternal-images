5fc36112b5e22c516ada3766753775d26e457028
Robert Sturla <robertsturla@outlook.com>
2024-07-05 00:19:18 +0100

    chore: remove zstd and reorder build scripts

--- lumina/scripts/_base/003-ptyxis-terminal.sh
+++ lumina/scripts/_base/003-ptyxis-terminal.sh
@@ -7,16 +7,6 @@ FEDORA_VERSION=$VERSION_ID
 
 wget https://copr.fedorainfracloud.org/coprs/ublue-os/staging/repo/fedora-$(rpm -E %fedora)/ublue-os-staging-fedora-$(rpm -E %fedora).repo -O /etc/yum.repos.d/_copr_ublue-os_staging.repo
 
-# Fedora 40 already has the required versions of gtk4 and libadwaita
-if [[ "$FEDORA_VERSION" == "39" ]]; then
-  rpm-ostree override replace \
-    --experimental \
-    --from repo=copr:copr.fedorainfracloud.org:ublue-os:staging \
-      gtk4 \
-      libadwaita \
-      vte291
-fi
-
 rpm-ostree install ptyxis
 
 rm -rf /etc/yum.repos.d/_copr_ublue-os_staging.repo
