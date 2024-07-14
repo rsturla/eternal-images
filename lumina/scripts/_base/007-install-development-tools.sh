0b111c501bb305d8e78d659a5f9a5836707accdf
Robert Sturla <robertsturla@outlook.com>
2024-07-14 10:23:45 +0100

    fix: strip duplicate zed.app directory

--- lumina/scripts/_base/007-install-development-tools.sh
+++ lumina/scripts/_base/007-install-development-tools.sh
@@ -32,7 +32,7 @@ install -c -m 0755 /tmp/devbox /usr/bin/devbox
 # Install Zed IDE
 curl -Lo /tmp/zed.tar.gz https://zed.dev/api/releases/stable/latest/zed-linux-x86_64.tar.gz
 mkdir -p /usr/lib/zed.app/
-tar -xvf /tmp/zed.tar.gz -C /usr/lib/zed.app/
+tar -xvf /tmp/zed.tar.gz -C /usr/lib/zed.app/ --strip-components=1
 ln -s /usr/lib/zed.app/bin/zed /usr/bin/zed
 cp /usr/lib/zed.app/share/applications/zed.desktop /usr/share/applications/zed.desktop
 sed -i "s|Icon=zed|Icon=/usr/lib/zed.app/share/icons/hicolor/512x512/apps/zed.png|g" /usr/share/applications/zed.desktop
