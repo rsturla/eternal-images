8ba1c9262faac9498083d92787b783aa96870570
Robert Sturla <robertsturla@outlook.com>
2024-07-26 23:34:29 +0100

    Remove ublue copr for ptyxis install

--- lumina/scripts/_base/003-ptyxis-terminal.sh
+++ lumina/scripts/_base/003-ptyxis-terminal.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
 set -euox pipefail
-source /etc/os-release
-
-FEDORA_VERSION=$VERSION_ID
-
-wget https://copr.fedorainfracloud.org/coprs/ublue-os/staging/repo/fedora-$(rpm -E %fedora)/ublue-os-staging-fedora-$(rpm -E %fedora).repo -O /etc/yum.repos.d/_copr_ublue-os_staging.repo
 
 rpm-ostree install ptyxis
-
-rm -rf /etc/yum.repos.d/_copr_ublue-os_staging.repo
