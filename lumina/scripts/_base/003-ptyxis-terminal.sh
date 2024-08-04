c3577af17ac39e9c6ab01f0d9570212584fe7a29
Robert Sturla <robertsturla@outlook.com>
2024-08-04 16:03:23 +0100

    Only use COPR for 39

--- lumina/scripts/_base/003-ptyxis-terminal.sh
+++ lumina/scripts/_base/003-ptyxis-terminal.sh
@@ -2,4 +2,17 @@
 
 set -euox pipefail
 
+FEDORA_VERSION=$VERSION_ID
+
+if [[ "$FEDORA_VERSION" == "39" ]]; then
+  # Add the COPR repository
+  wget https://copr.fedorainfracloud.org/coprs/ublue-os/staging/repo/fedora-$(rpm -E %fedora)/ublue-os-staging-fedora-$(rpm -E %fedora).repo -O /etc/yum.repos.d/_copr_ublue-os_staging.repo
+fi
+
+# Install the package
 rpm-ostree install ptyxis
+
+# Check if the Fedora version is 39
+if [[ "$FEDORA_VERSION" == "39" ]]; then
+  rm -rf /etc/yum.repos.d/_copr_ublue-os_staging.repo
+fi
