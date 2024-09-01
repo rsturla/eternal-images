215be0341daee0dd5d8199953e694168576d44cc
Robert Sturla <robertsturla@outlook.com>
2024-09-01 11:32:28 +0100

    fix: add user to wireguard group

--- nebula/scripts/_base/003-ptyxis-terminal.sh
+++ nebula/scripts/_base/003-ptyxis-terminal.sh
@@ -1,19 +1,6 @@
 #!/usr/bin/env bash
 
 set -euox pipefail
-source /etc/os-release
-
-FEDORA_VERSION=$VERSION_ID
-
-if [[ "$FEDORA_VERSION" == "39" ]]; then
-  # Add the COPR repository
-  wget https://copr.fedorainfracloud.org/coprs/ublue-os/staging/repo/fedora-$(rpm -E %fedora)/ublue-os-staging-fedora-$(rpm -E %fedora).repo -O /etc/yum.repos.d/_copr_ublue-os_staging.repo
-fi
 
 # Install the package
 rpm-ostree install ptyxis
-
-# Check if the Fedora version is 39
-if [[ "$FEDORA_VERSION" == "39" ]]; then
-  rm -rf /etc/yum.repos.d/_copr_ublue-os_staging.repo
-fi
