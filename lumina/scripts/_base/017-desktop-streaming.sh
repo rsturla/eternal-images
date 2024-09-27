7312359bd8cd5ab8b13b25d550b8f1ea04457615
Robert Sturla <robertsturla@outlook.com>
2024-09-27 14:57:43 +0100

    fix: use correct version variable

--- lumina/scripts/_base/017-desktop-streaming.sh
+++ lumina/scripts/_base/017-desktop-streaming.sh
@@ -5,7 +5,7 @@ set -euox pipefail
 source /etc/os-release
 
 FEDORA_VERSION=$VERSION_ID
-curl -Lo /etc/yum.repos.d/matte-schwartz-sunshine.repo https://copr.fedorainfracloud.org/coprs/matte-schwartz/sunshine/repo/fedora-${FEDORA_VERSION}/matte-schwartz-sunshine-fedora-"${FEDORA_MAJOR_VERSION}".repo && \
+curl -Lo /etc/yum.repos.d/matte-schwartz-sunshine.repo "https://copr.fedorainfracloud.org/coprs/matte-schwartz/sunshine/repo/fedora-${FEDORA_VERSION}/matte-schwartz-sunshine-fedora-${FEDORA_VERSION}.repo" && \
 
 rpm-ostree install sunshine
 systemctl enable sunshine-workaround.service
