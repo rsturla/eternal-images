c81306f0b1d459252241dcf456c41356e4332080
Robert Sturla <robertsturla@outlook.com>
2024-10-01 11:18:05 +0100

    fix: remove gnome-terminal-nautilus in F40 only

--- lumina/scripts/_silverblue/002-remove-gnome-packages.sh
+++ lumina/scripts/_silverblue/002-remove-gnome-packages.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
 set -euox pipefail
+source /etc/os-release
+
+FEDORA_VERSION=$VERSION_ID
 
 rpm-ostree override remove \
   gnome-tour \
   gnome-extensions-app \
   gnome-system-monitor \
-  gnome-terminal-nautilus \
   yelp
+
+if [[ "$FEDORA_VERSION" == "40" ]]; then
+  rpm-ostree override remove gnome-terminal-nautilus
+fi
