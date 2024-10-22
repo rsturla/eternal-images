2fe4b9d2dcad025f75bdf90588d484359cb3baa8
Robert Sturla <robertsturla@outlook.com>
2024-10-22 23:50:02 +0100

    chore: install in ostree mode

--- lumina/files/_base/etc/eternal/setup-manager/system.d/nix.sh
+++ lumina/files/_base/etc/eternal/setup-manager/system.d/nix.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm --determinate
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install ostree --no-confirm --determinate
 
 tee /etc/sudoers.d/nix-sudo-env <<EOF
     Defaults  secure_path = /nix/var/nix/profiles/default/bin:/nix/var/nix/profiles/default/sbin:$(printenv PATH)
