6691d62f0ac9a43c2a32e4c3c2d04cd4c4389c5d
Robert Sturla <robertsturla@outlook.com>
2024-10-22 23:47:09 +0100

    feature: install nix in determinate mode

--- lumina/files/_base/etc/eternal/setup-manager/system.d/nix.sh
+++ lumina/files/_base/etc/eternal/setup-manager/system.d/nix.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm --determinate
 
 tee /etc/sudoers.d/nix-sudo-env <<EOF
     Defaults  secure_path = /nix/var/nix/profiles/default/bin:/nix/var/nix/profiles/default/sbin:$(printenv PATH)
