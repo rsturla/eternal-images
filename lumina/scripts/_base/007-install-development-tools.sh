ac05ba506aa9dc7bd175bbfac1900492247af709
Robert Sturla <robertsturla@outlook.com>
2024-11-01 23:11:42 +0000

    chore: prune unused packages (#322)
    * chore: prune unused packages
    
    * chore: remove F40 builds

--- lumina/scripts/_base/007-install-development-tools.sh
+++ lumina/scripts/_base/007-install-development-tools.sh
@@ -23,21 +23,11 @@ EOF
 # Install development tools
 rpm-ostree install \
   code \
-  $(curl https://api.github.com/repos/wagoodman/dive/releases/latest | jq -r '.assets[] | select(.name| test(".*_linux_amd64.rpm$")).browser_download_url') \
   gh \
   git-credential-oauth
 
 curl -Lo /tmp/devbox https://releases.jetpack.io/devbox
 install -c -m 0755 /tmp/devbox /usr/bin/devbox
-
-# Install Zed IDE
-curl -Lo /tmp/zed.tar.gz https://zed.dev/api/releases/stable/latest/zed-linux-x86_64.tar.gz
-mkdir -p /usr/lib/zed.app/
-tar -xvf /tmp/zed.tar.gz -C /usr/lib/zed.app/ --strip-components=1
-ln -s /usr/lib/zed.app/bin/zed /usr/bin/zed
-cp /usr/lib/zed.app/share/applications/zed.desktop /usr/share/applications/dev.zed.Zed.desktop
-sed -i "s|Icon=zed|Icon=/usr/lib/zed.app/share/icons/hicolor/512x512/apps/zed.png|g" /usr/share/applications/dev.zed.Zed.desktop
-sed -i "s|Exec=zed|Exec=/usr/lib/zed.app/libexec/zed-editor|g" /usr/share/applications/dev.zed.Zed.desktop
 
 rm -f /etc/yum.repos.d/github.repo
 rm -f /etc/yum.repos.d/vscode.repo
