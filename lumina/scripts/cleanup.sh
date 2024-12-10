98f800d27f8aecdde8c5fed062dce13b4fbdcbe9
Robert Sturla <robertsturla@outlook.com>
2024-12-10 01:00:20 +0000

    chore: add bootc container lint (#339)

--- lumina/scripts/cleanup.sh
+++ lumina/scripts/cleanup.sh
@@ -3,3 +3,6 @@
 set -euox pipefail
 
 sed -i 's/enabled=1/enabled=0/g' /etc/yum.repos.d/rpmfusion-{free,nonfree}{,-updates,-updates-testing}.repo
+
+# Clear out unsupported directories
+rm -rf /tmp/* /var/*
