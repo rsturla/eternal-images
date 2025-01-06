59efe9d7be0286b4aa55cdfaaed460aab5ea916c
Robert Sturla <robertsturla@outlook.com>
2025-01-06 18:45:48 +0000

    chore: enable GHA log grouping

--- lumina/scripts/setup.sh
+++ lumina/scripts/setup.sh
@@ -26,14 +26,16 @@ fi
 
 for script in /tmp/scripts/_base/*.sh; do
   if [[ -f "$script" ]]; then
-    echo "Running $script"
+    echo "::group:: ===$(basename "$script")==="
     bash "$script"
+    echo "::endgroup::"
   fi
 done
 
 for script in /tmp/scripts/_$BASE/*.sh; do
   if [[ -f "$script" ]]; then
-    echo "Running $script"
+    echo "::group:: ===$(basename "$script")==="
     bash "$script"
+    echo "::endgroup::"
   fi
 done
