4f138db892cedaf3eaf3dcb6aaf7ef6c22147cc3
Robert Sturla <robertsturla@outlook.com>
2025-02-06 13:09:38 +0000

    fix: workaround Google Chrome profile locks (#353)
    * fix: workaround Google Chrome profile locks
    
    * fix: correct Singleton file glob

--- lumina/scripts/_base/009-install-google-chrome.sh
+++ lumina/scripts/_base/009-install-google-chrome.sh
@@ -1,22 +1,9 @@
 #!/usr/bin/env sh
 
-# Thanks to bri for the inspiration! My script is based on this example:
-# https://github.com/briorg/bluefin/blob/c62c30a04d42fd959ea770722c6b51216b4ec45b/scripts/1password.sh
-
 set -ouex pipefail
 
-echo "Installing Google Chrome"
-
-# On libostree systems, /opt is a symlink to /var/opt,
-# which actually only exists on the live system. /var is
-# a separate mutable, stateful FS that's overlaid onto
-# the ostree rootfs. Therefore we need to install it into
-# /usr/lib/google instead, and dynamically create a
-# symbolic link /opt/google => /usr/lib/google upon
-# boot.
-
 # Prepare staging directory
-mkdir -p /var/opt # -p just in case it exists
+mkdir -p /var/opt
 
 # Setup repo
 cat << EOF > /etc/yum.repos.d/google-chrome.repo
@@ -38,18 +25,21 @@ curl --retry 3 --retry-delay 2 --retry-all-errors -sL \
   https://dl.google.com/linux/linux_signing_key.pub
 rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-google
 
-# Now let's install the packages.
+# Install the packages
 dnf install -y google-chrome-stable
 
-# Clean up the yum repo (updates are baked into new images)
-rm /etc/yum.repos.d/google-chrome.repo -f
+# Clean up the yum repo
+rm -f /etc/yum.repos.d/google-chrome.repo
 
-# And then we do the hacky dance!
-mv /var/opt/google /usr/lib/google # move this over here
+# Move the application to somewhere on the final image
+mv /var/opt/google /usr/lib/google
 
-#####
 # Register path symlink
-# We do this via tmpfiles.d so that it is created by the live system.
 cat >/usr/lib/tmpfiles.d/eternal-google.conf <<EOF
 L  /opt/google  -  -  -  -  /usr/lib/google
+EOF
+
+# Workaround to remove Google Chrome profile locks
+cat >/usr/share/user-tmpfiles.d/eternal-google-locks.conf <<EOF
+r  %h/.config/google-chrome/Singleton*  -  -  -  -  -
 EOF
