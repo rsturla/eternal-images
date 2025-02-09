15162274a0f19b5e8b9fdce677c05c9f1208d8e9
Robert Sturla <robertsturla@outlook.com>
2025-02-09 00:30:17 +0000

    feat: install cloud-provider-kind (#359)
    * feat: install cloud-provider-kind
    
    * fix: download location

--- lumina/scripts/_base/007-install-development-tools.sh
+++ lumina/scripts/_base/007-install-development-tools.sh
@@ -26,9 +26,15 @@ dnf install -y \
   gh \
   git-credential-oauth
 
+# Install Kind
 curl --retry 3 -Lo /tmp/kind https://github.com/kubernetes-sigs/kind/releases/latest/download/kind-linux-amd64
 install -c -m 0755 /tmp/kind /usr/bin/kind
+DOWNLOAD_URL=$(curl https://api.github.com/repos/kubernetes-sigs/cloud-provider-kind/releases/latest | jq -r '.assets[] | select(.name| test("_linux_amd64.tar.gz$")).browser_download_url')
+curl --retry 3 -Lo /tmp/cloud-provider-kind.tar.gz $DOWNLOAD_URL
+tar -xzf /tmp/cloud-provider-kind.tar.gz -C /tmp
+install -c -m 0755 /tmp/cloud-provider-kind /usr/bin/cloud-provider-kind
 
+# Install Devbox
 curl --retry 3 -Lo /tmp/devbox https://releases.jetpack.io/devbox
 install -c -m 0755 /tmp/devbox /usr/bin/devbox
 
