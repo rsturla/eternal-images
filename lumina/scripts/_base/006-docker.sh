da8f25604d4a7f8c7611c0eb0b386570ef841a41
Robert Sturla <robertsturla@outlook.com>
2025-04-15 18:00:59 +0100

    chore: enable testing repos for F42

--- lumina/scripts/_base/006-docker.sh
+++ lumina/scripts/_base/006-docker.sh
@@ -7,12 +7,25 @@ cat << EOF > /etc/yum.repos.d/docker-ce.repo
 [docker-ce-stable]
 name=Docker CE Stable
 baseurl=https://download.docker.com/linux/fedora/\$releasever/\$basearch/stable
-enabled=1
+enabled=0
+gpgcheck=1
+gpgkey=https://download.docker.com/linux/fedora/gpg
+
+[docker-ce-testing]
+name=Docker CE Testing - $basearch
+baseurl=https://download.docker.com/linux/fedora/$releasever/$basearch/test
+enabled=0
 gpgcheck=1
 gpgkey=https://download.docker.com/linux/fedora/gpg
 EOF
 
-dnf install -y \
+DOCKER_REPO=stable
+FEDORA_VERSION=$(rpm -E %fedora)
+if [[ "$FEDORA_VERSION" -eq 42 ]]; then
+    DOCKER_REPO=testing
+fi
+
+dnf install -y --enablerepo=docker-ce-$DOCKER_REPO \
   docker-ce \
   docker-ce-cli \
   containerd.io \
