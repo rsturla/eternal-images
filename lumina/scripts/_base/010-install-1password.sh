ed1814068facbeeb5cba8757764b25ef1db8c1d9
Robert Sturla <robertsturla@outlook.com>
2025-10-19 11:19:43 +0100

    fix: 1password & 1password-cli GIDs (#415)

--- lumina/scripts/_base/010-install-1password.sh
+++ lumina/scripts/_base/010-install-1password.sh
@@ -39,8 +39,33 @@ repo_gpgcheck=1
 gpgkey=https://downloads.1password.com/linux/keys/1password.asc
 EOF
 
-# Import signing key
-#rpm --import https://downloads.1password.com/linux/keys/1password.asc
+# Normally, after-install.sh would create a group,
+# "onepassword", right about now. But if we do that during
+# the ostree build it'll disappear from the running system!
+# I'm going to work around that by hardcoding GIDs and
+# crossing my fingers that nothing else steps on them.
+# These numbers _should_ be okay under normal use, but
+# if there's a more specific range that I should use here
+# please submit a PR!
+
+# Specifically, GID must be > 1000, and absolutely must not
+# conflict with any real groups on the deployed system.
+# Normal user group GIDs on Fedora are sequential starting
+# at 1000, so let's skip ahead and set to something higher.
+GID_ONEPASSWORD="1790"
+GID_ONEPASSWORDCLI="1791"
+
+
+cat >/usr/lib/sysusers.d/onepassword.conf <<EOF
+g onepassword ${GID_ONEPASSWORD}
+EOF
+
+cat >/usr/lib/sysusers.d/onepassword-cli.conf <<EOF
+g onepassword-cli ${GID_ONEPASSWORDCLI}
+EOF
+
+systemd-sysusers /usr/lib/sysusers.d/onepassword.conf
+systemd-sysusers /usr/lib/sysusers.d/onepassword-cli.conf
 
 # Now let's install the packages.
 dnf install -y 1password 1password-cli
@@ -59,27 +84,6 @@ ln -s /opt/1Password/1password /usr/bin/1password
 # The following is a bastardization of "after-install.sh"
 # which is normally packaged with 1password. You can compare with
 # /usr/lib/1Password/after-install.sh if you want to see.
-
-# chrome-sandbox requires the setuid bit to be specifically set.
-# See https://github.com/electron/electron/issues/17972
-chmod 4755 /usr/lib/1Password/chrome-sandbox
-
-# Normally, after-install.sh would create a group,
-# "onepassword", right about now. But if we do that during
-# the ostree build it'll disappear from the running system!
-# I'm going to work around that by hardcoding GIDs and
-# crossing my fingers that nothing else steps on them.
-# These numbers _should_ be okay under normal use, but
-# if there's a more specific range that I should use here
-# please submit a PR!
-
-# Specifically, GID must be > 1000, and absolutely must not
-# conflict with any real groups on the deployed system.
-# Normal user group GIDs on Fedora are sequential starting
-# at 1000, so let's skip ahead and set to something higher.
-GID_ONEPASSWORD="1790"
-GID_ONEPASSWORDCLI="1791"
-
 BROWSER_SUPPORT_PATH="/usr/lib/1Password/1Password-BrowserSupport"
 
 # BrowserSupport binary needs setgid. This gives no extra permissions to the binary.
@@ -91,22 +95,11 @@ chmod g+s "${BROWSER_SUPPORT_PATH}"
 chgrp ${GID_ONEPASSWORDCLI} /usr/bin/op
 chmod g+s /usr/bin/op
 
-# Dynamically create the required groups via sysusers.d
-# and set the GID based on the files we just chgrp'd
-cat >/usr/lib/sysusers.d/onepassword.conf <<EOF
-g onepassword ${GID_ONEPASSWORD}
-EOF
-cat >/usr/lib/sysusers.d/onepassword-cli.conf <<EOF
-g onepassword-cli ${GID_ONEPASSWORDCLI}
-EOF
-
-# remove the sysusers.d entries created by onepassword RPMs.
-# They don't magically set the GID like we need them to.
-rm -f /usr/lib/sysusers.d/30-rpmostree-pkg-group-onepassword.conf
-rm -f /usr/lib/sysusers.d/30-rpmostree-pkg-group-onepassword-cli.conf
-
 # Register path symlink
 # We do this via tmpfiles.d so that it is created by the live system.
 cat >/usr/lib/tmpfiles.d/eternal-onepassword.conf <<EOF
 L  /opt/1Password  -  -  -  -  /usr/lib/1Password
 EOF
+
+getent group onepassword
+getent group onepassword-cli
