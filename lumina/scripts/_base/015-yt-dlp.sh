7f9e2ad620d70772d81bb3bb2149f5e49b22bc63
Robert Sturla <robertsturla@outlook.com>
2025-10-19 22:24:05 +0100

    chore: move yt-dlp to brew bundle (#417)

--- lumina/scripts/_base/015-yt-dlp.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-set -euox pipefail
-
-# Download yt-dlp from GitHub releases
-ghcurl "https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp" -o /tmp/yt-dlp
-install -c -m 0755 /tmp/yt-dlp /usr/bin/yt-dlp
