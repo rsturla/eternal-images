615c798b0ee13b00028bcf1b958543c710ae489c
Robert Sturla <robertsturla@outlook.com>
2025-10-21 08:26:07 +0100

    feat: install claude code dependencies

--- lumina/scripts/_base/007-install-development-tools.sh
+++ lumina/scripts/_base/007-install-development-tools.sh
@@ -12,8 +12,23 @@ gpgcheck=1
 gpgkey=https://packages.microsoft.com/keys/microsoft.asc
 EOF
 
+cat << EOF > /etc/yum.repos.d/google-cloud-sdk.repo
+[google-cloud-cli]
+name=Google Cloud CLI
+baseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el9-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=0
+gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
 # Install development tools
 dnf install -y \
-  code
+  code \
+  libxcrypt-compat \
+  google-cloud-cli \
+  nodejs \
+  ripgrep
 
 rm -f /etc/yum.repos.d/vscode.repo
+rm -f /etc/yum.repos.d/google-cloud-sdk.repo
