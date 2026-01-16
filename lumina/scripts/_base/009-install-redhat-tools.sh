9392b82e921e3b455c4c97b5e9e4eaee572d0648
Robert Sturla <robertsturla@outlook.com>
2026-01-16 11:58:03 +0000

    chore: install tmt tools

--- lumina/scripts/_base/009-install-redhat-tools.sh
+++ lumina/scripts/_base/009-install-redhat-tools.sh
@@ -5,4 +5,8 @@ set -euox pipefail
 dnf install -y \
     krb5-workstation \
     beaker-client \
-    conserver-client
+    conserver-client \
+    tmt
+
+curl -Lo /usr/bin/testing-farm "https://gitlab.com/testing-farm/cli/-/raw/main/container/testing-farm"
+chmod +x /usr/bin/testing-farm
