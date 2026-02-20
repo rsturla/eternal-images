0803cf3ede2bdaef5b062dc9576f6bd5b0997c29
Robert Sturla <rsturla@redhat.com>
2026-02-20 20:37:30 +0000

    lumina: rechunk with chunkah

--- lumina/scripts/_base/004-terminal.sh
+++ lumina/scripts/_base/004-terminal.sh
@@ -17,3 +17,7 @@ git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git \
 
 # Remove .git directories to save space
 find /usr/share/oh-my-zsh -name '.git' -type d -exec rm -rf {} + 2>/dev/null || true
+
+# Chunkah: group omz + plugins (non-RPM) so they get their own layer
+setfattr -n user.component -v "lumina-omz" /usr/share/oh-my-zsh
+find /usr/share/oh-my-zsh -mindepth 1 -exec setfattr -n user.component -v "lumina-omz" {} \;
