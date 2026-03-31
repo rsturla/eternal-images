382b2921af5b42bc30c2385235a3c5c288ed7a7b
Robert Sturla <robertsturla@outlook.com>
2026-03-31 15:41:37 +0100

    chore: install netbird

--- lumina/scripts/_base/009-install-redhat-tools.sh
+++ lumina/scripts/_base/009-install-redhat-tools.sh
@@ -6,7 +6,8 @@ dnf install -y \
     krb5-workstation \
     beaker-client \
     conserver-client \
-    tmt
+    tmt \
+    netbird
 
 curl -Lo /usr/bin/testing-farm "https://gitlab.com/testing-farm/cli/-/raw/main/container/testing-farm"
 chmod +x /usr/bin/testing-farm
