be5a055e38436b50c567bc37faa16d20b91a1fc4
Robert Sturla <robertsturla@outlook.com>
2026-04-01 01:41:58 +0100

    fix: include netbird repo

--- lumina/scripts/_base/009-install-redhat-tools.sh
+++ lumina/scripts/_base/009-install-redhat-tools.sh
@@ -2,12 +2,25 @@
 
 set -euox pipefail
 
+cat << EOF > /etc/yum.repos.d/netbird.repo
+[NetBird]
+name=NetBird
+baseurl=https://pkgs.netbird.io/yum/
+enabled=1
+gpgcheck=0
+gpgkey=https://pkgs.netbird.io/yum/repodata/repomd.xml.key
+repo_gpgcheck=1
+EOF
+
 dnf install -y \
     krb5-workstation \
     beaker-client \
     conserver-client \
     tmt \
-    netbird
+    netbird \
+    netbird-ui
 
 curl -Lo /usr/bin/testing-farm "https://gitlab.com/testing-farm/cli/-/raw/main/container/testing-farm"
 chmod +x /usr/bin/testing-farm
+
+rm -f /etc/yum.repos.d/netbird.repo
